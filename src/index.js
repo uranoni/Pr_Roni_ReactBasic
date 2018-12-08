19a88e2dab1b33819cad38721c4f9f419117191d
uranoni <uranoni777@gmail.com>
2018-12-08 15:28:33 +0800

    setstate function basic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import List from './List'
+import Message from './Message'
 
-ReactDOM.render(<List/>,document.getElementById('root'))
+
+ReactDOM.render(<Message/>,document.getElementById('root'))
